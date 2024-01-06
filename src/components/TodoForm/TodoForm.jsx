0d741e222a5aa8ddbd19974176653a850608c668
Raja Gupta <erraja97@gmail.com>
2024-01-06 13:30:06 +0530

    Updated Dependency for Category Dropdown

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -38,7 +38,7 @@ function TodoForm({ onComponentChange }) {
     };
   
     fetchTodosByCategory();
-  }, [user, handleCreateTodo, category]); // Dependency on user to run the effect when the user changes
+  }, [user, handleCreateTodo]); // Dependency on user to run the effect when the user changes
   
 
   const handleCreateTodo = async () => {
